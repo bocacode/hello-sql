8bffacc978cb10a28975db4374d9ec532e906e7d
Todd Albert <todd@bocacode.com>
2022-07-20 16:46:00 -0400

    connected to mysql; return books

---
+++ index.js
@@ -0,0 +1,21 @@
+// import mysql2 library
+const mysql = require('mysql2');
+
+// connect to our database
+const db = mysql.createConnection({
+  host: '34.170.135.140',
+  user: 'root',
+  password: 'BuildingSoFLo',
+  database: 'swecc-summer-2022'
+});
+
+// run a simple query
+db.query('SELECT * FROM Books_dd', (err, results) => {
+  if(err) {
+    console.log(err);
+  }
+  // output the results
+  console.log(results);
+  db.end();
+});
+
